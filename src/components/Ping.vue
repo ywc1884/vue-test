e550087d81e89641bfff40b46a9a55c50e82db73
benjamin yin <yinwencai@gmail.com>
2021-01-21 21:01:17 +0800

    initial upload

---
+++ src/components/Ping.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    Ping result: {{ msg }}
+    <div>
+      <el-button type="primary" id="apiButton" @click="this.getMessage">call test api</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Ping',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  // 生命周期函数，加载页面时会先执行这个函数，从而调用 getMessage() 方法
+  // created () {
+  //   this.getMessage()
+  // },
+  methods: {
+    getMessage () {
+      // 使用 axios 向 flask 发送请求
+      const url = 'http://127.0.0.1:5000/api/ping'
+      axios.get(url)
+        .then((res) => {
+          this.msg = res.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+#apiButton {
+  margin-top: 10px;
+}
+</style>
